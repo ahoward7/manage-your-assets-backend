6e5b0e6cbeb6f69855d7cd9f459015b40aec381b
Avery Howard <avery.d.howard@gmail.com>
2025-02-23 21:14:26 -0500

    Cleanup

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -36,8 +36,6 @@ router.post('/google', async (req, res) => {
 
     const potentialAccount = await Account.findOne({ email })
 
-    console.log(potentialAccount)
-
     if (!potentialAccount) {
       const userFromGoogle = userFromGoogleAccount(googleAccount)
       const accountFromGoogle = accountFromGoogleAccount(googleAccount)
@@ -55,12 +53,8 @@ router.post('/google', async (req, res) => {
     if (potentialAccount.client === 'mya') {
       const mergedAccount = { ...potentialAccount.toObject(), client: 'merged' }
 
-      console.log(mergedAccount)
-
       await Account.updateOne({ email }, mergedAccount)
       await User.updateOne({ _id: potentialAccount.user }, { image: picture })
-
-      console.log('updated')
 
       res.status(200).json(mergedAccount)
       return
@@ -93,7 +87,14 @@ router.post('/login', async (req, res) => {
       return
     }
 
-    res.status(200).json(potentialAccount)
+    const user = await User.findById(potentialAccount.user)
+
+    if (!user) {
+      res.status(404).send('User not found with this account')
+      return
+    }
+
+    res.status(200).json(user)
   }
   catch (error) {
     res.status(500).send(error.message)
