d04bfed1df599393c7ac293ddde527ff35293d61
Avery Howard <avery.d.howard@gmail.com>
2025-02-25 20:01:08 -0500

    Return user instead of account for login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -67,7 +67,14 @@ router.post('/google', async (req, res) => {
       return
     }
 
-    res.status(200).json(potentialAccount)
+    const user = await User.findById(potentialAccount.user)
+
+    if (!user) {
+      res.status(404).send('User not found with this account')
+      return
+    }
+
+    res.status(200).json(user)
   }
   catch (error) {
     res.status(500).send(error.message)
