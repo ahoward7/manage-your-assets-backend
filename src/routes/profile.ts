d86657976e421ce3b49ff9c85ae9e63655c5e48b
Avery Howard <avery.d.howard@gmail.com>
2025-03-16 22:06:57 -0400

    Move profile routes

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,17 +1,55 @@
 import { Router } from 'express'
+import Profile from '../models/Profile'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('get')
+router.get('/profile', async (req, res) => {
+  try {
+    const { user } = req.query
+
+    if (!user) {
+      res.status(400).send('User id is required')
+      return
+    }
+
+    const profile = await Profile.findOne({ user })
+
+    if (!profile) {
+      res.status(404).send('Profile not found')
+      return
+    }
+
+    res.status(200).json(profile)
+  }
+  catch (error) {
+    res.status(500).send(error.message)
+  }
 })
 
 router.post('/', (req, res) => {
   res.send('post')
 })
 
-router.put('/', (req, res) => {
-  res.send('put')
+router.put('/profile', async (req, res) => {
+  try {
+    const profile = req.body
+
+    const potentialProfile = await Profile.findOne({ user: profile.user })
+
+    if (potentialProfile) {
+      const updatedProfile = await Profile.updateOne({ user: profile.user }, profile)
+
+      if (updatedProfile) {
+        res.status(200).send('Profile updated')
+        return
+      }
+
+      res.status(500).send('Failed to update profile')
+    }
+  }
+  catch (error) {
+    res.status(500).send(error.message)
+  }
 })
 
 router.delete('/', (req, res) => {
